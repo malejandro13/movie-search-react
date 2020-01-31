0049370df1030538eb7efb55a8a9daae2f702275
Marcelo Soto <malejandro13@Marcelos-MacBook-Air.local>
2020-01-31 18:20:08 -0300

    using index on the key to avoid duplication of the key

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -10,9 +10,9 @@ const MoviesList = ({ Search, totalResults }) => (
             {
                 (!Search)
                     ? (<h1 className="w-full text-3xl text-center pt-10">No se encuentran registros <Emoji symbol="🙄" /></h1>)
-                    : Search.map((movie) => (
+                    : Search.map((movie, index) => (
 
-                        <div key={movie.imdbID} className="w-full sm:w-1/3 md:w-1/4 flex flex-col p-3" style={{ height: "400px" }}>
+                        <div key={`${movie.imdbID}-${index}`} className="w-full sm:w-1/3 md:w-1/4 flex flex-col p-3" style={{ height: "400px" }}>
                             <MovieCard
                                 {...movie}
                             />
@@ -24,6 +24,4 @@ const MoviesList = ({ Search, totalResults }) => (
 
     </Fragment >
 )
-
-
 export default MoviesList
