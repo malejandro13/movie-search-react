04e1de147567601bfaf13f42543858395bcecdaa
Marcelo Soto <malejandro13@Marcelos-MacBook-Air.local>
2020-02-02 23:40:17 -0300

    adding GitHub Project link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import Emoji from './Emoji'
 const Footer = ({ Search, totalResults }) => (
     <div className="w-full bg-teal-500 text-center object-bottom text-gray-200 text-xs py-5">
         <p>Use of <a href="https://www.omdbapi.com/" target="_blank" rel="noopener noreferrer">OMDb API</a> for results</p>
-        <p>Learning React JS <Emoji symbol="❤️" /></p>
+        <p>Learning React JS <Emoji symbol="❤️" /> - <a href="https://github.com/malejandro13/movie-search-react" target="_blank" rel="noopener noreferrer">GitHub</a></p>
         <p>
             <a href="https://www.udemy.com/course/aprendiendo-react/" target="_blank" rel="noopener noreferrer">
                 do you want to learn React JS? (Spanish)
